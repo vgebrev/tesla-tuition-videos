9027ec930dc89269d7af8d00b1caafe0c986e908
Veli Gebrev <veli@innosys.co.za>
2023-08-14 14:06:52 +0200

    feat: Add some logging to the TagController

--- TTV.Web/Api/Controllers/TagController.cs
+++ TTV.Web/Api/Controllers/TagController.cs
@@ -8,16 +8,19 @@ namespace TTV.Web.Api.Controllers
     [ApiController]
     public class TagController : ControllerBase
     {
+        private readonly ILogger<TagController> logger;
         private readonly ITagDataService dataService;
 
-        public TagController(ITagDataService dataService)
+        public TagController(ILogger<TagController> logger, ITagDataService dataService)
         {
+            this.logger = logger;
             this.dataService = dataService;
         }
 
         [HttpGet]
         public async Task<IEnumerable<TagDto>> GetTagsAsync(CancellationToken cancellationToken = default)
         {
+            logger.LogInformation("{Method}()", nameof(GetTagsAsync));
             return await dataService.GetTagsAsync(cancellationToken);
         }
     }

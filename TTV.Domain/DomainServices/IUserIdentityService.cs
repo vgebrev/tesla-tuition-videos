d6981187ae0489d5870dc409b280609ca924e31b
Veli Gebrev <veli@innosys.co.za>
2023-08-15 13:39:06 +0200

    feat: Add a user identity service, so inner layers can make decisions based on the user's identity

---
+++ TTV.Domain/DomainServices/IUserIdentityService.cs
@@ -0,0 +1,9 @@
+﻿namespace TTV.Domain.DomainServices;
+
+public interface IUserIdentityService
+{
+    public Guid? UserId { get; }
+    public string? Email { get; }
+    public bool IsAuthenticated { get; }
+    public bool IsAnonymous { get; }
+}
---
+++ TTV.Web/Api/ClaimsIdentityService.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using TTV.Domain.DomainServices;
+
+namespace TTV.Web.Api;
+
+public class ClaimsIdentityService : IUserIdentityService
+{
+    public ClaimsIdentityService(IHttpContextAccessor httpContextAccessor)
+    {
+        var user = httpContextAccessor.HttpContext?.User;
+        if (Guid.TryParse(user?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out var userId))
+        {
+            UserId = userId;
+        }
+        Email = user?.FindFirst(ClaimTypes.Email)?.Value;
+
+    }
+    public Guid? UserId { get; }
+
+    public string? Email { get; }
+
+    public bool IsAuthenticated => !string.IsNullOrEmpty(Email);
+
+    public bool IsAnonymous => !IsAuthenticated;
+}

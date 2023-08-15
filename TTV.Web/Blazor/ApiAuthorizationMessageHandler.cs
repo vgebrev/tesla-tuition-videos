901e4f4bdc92857b7cf93972f8e65141ae0c12cd
Veli Gebrev <veli@innosys.co.za>
2023-08-15 13:37:44 +0200

    fix: Http request handler no longer throws when no access token is available

--- TTV.Web/Blazor/ApiAuthorizationMessageHandler.cs
+++ TTV.Web/Blazor/ApiAuthorizationMessageHandler.cs
@@ -1,16 +1,113 @@
-﻿namespace TTV.Web.Blazor;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
 
-public class ApiAuthorizationMessageHandler : AuthorizationMessageHandler
+namespace TTV.Web.Blazor;
+
+public sealed class ApiAuthorizationMessageHandler : DelegatingHandler, IDisposable
 {
-    public ApiAuthorizationMessageHandler(IAccessTokenProvider provider,
-        NavigationManager navigation, IOptionsSnapshot<LocalConfig> options)
-        : base(provider, navigation)
+    private readonly IAccessTokenProvider provider;
+    private readonly NavigationManager navigation;
+    private readonly AuthenticationStateChangedHandler? authenticationStateChangedHandler;
+    
+    private AccessToken? lastToken;
+    private AuthenticationHeaderValue? cachedHeader;
+    private Uri[]? authorizedUris;
+    private AccessTokenRequestOptions? tokenOptions;
+
+    public ApiAuthorizationMessageHandler(
+        IAccessTokenProvider provider,
+        NavigationManager navigation,
+        IOptionsSnapshot<LocalConfig> options)
     {
+        this.provider = provider;
+        this.navigation = navigation;
+
+        if (this.provider is AuthenticationStateProvider authStateProvider)
+        {
+            authenticationStateChangedHandler = _ => { lastToken = null; };
+            authStateProvider.AuthenticationStateChanged += authenticationStateChangedHandler;
+        }
+
         ConfigureHandler(
             authorizedUrls: new[] { options.Value.ApiRootUri },
             scopes: options.Value.ApiScopes);
     }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var now = DateTimeOffset.Now;
+        if (authorizedUris == null)
+        {
+            throw new InvalidOperationException($"The '{nameof(AuthorizationMessageHandler)}' is not configured. " +
+                $"Call '{nameof(AuthorizationMessageHandler.ConfigureHandler)}' and provide a list of endpoint urls to attach the token to.");
+        }
+
+        if (request.RequestUri != null && authorizedUris.Any(uri => uri.IsBaseOf(request.RequestUri)))
+        {
+            if (lastToken == null || now >= lastToken.Expires.AddMinutes(-5))
+            {
+                var tokenResult = tokenOptions != null ?
+                    await provider.RequestAccessToken(tokenOptions) :
+                    await provider.RequestAccessToken();
+
+                if (tokenResult.TryGetToken(out var token))
+                {
+                    lastToken = token;
+                    cachedHeader = new AuthenticationHeaderValue("Bearer", lastToken.Value);
+                }
+            }
+
+            request.Headers.Authorization = cachedHeader;
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+
+    public ApiAuthorizationMessageHandler ConfigureHandler(
+        IEnumerable<string> authorizedUrls,
+        IEnumerable<string>? scopes = null,
+        string? returnUrl = null)
+    {
+        if (authorizedUris != null)
+        {
+            throw new InvalidOperationException("Handler already configured.");
+        }
+
+        if (authorizedUrls == null)
+        {
+            throw new ArgumentNullException(nameof(authorizedUrls));
+        }
+
+        var uris = authorizedUrls.Select(uri => new Uri(uri, UriKind.Absolute)).ToArray();
+        if (uris.Length == 0)
+        {
+            throw new ArgumentException("At least one URL must be configured.", nameof(authorizedUrls));
+        }
+
+        authorizedUris = uris;
+        var scopesList = scopes?.ToArray();
+        if (scopesList != null || returnUrl != null)
+        {
+            tokenOptions = new AccessTokenRequestOptions
+            {
+                Scopes = scopesList,
+                ReturnUrl = returnUrl
+            };
+        }
+
+        return this;
+    }
+
+    void IDisposable.Dispose()
+    {
+        if (provider is AuthenticationStateProvider authStateProvider)
+        {
+            authStateProvider.AuthenticationStateChanged -= authenticationStateChangedHandler;
+        }
+        Dispose(disposing: true);
+    }
 }
+

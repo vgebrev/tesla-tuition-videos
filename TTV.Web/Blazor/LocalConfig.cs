685482edeed2a6692bee0e4a2a7f57b6393be575
Veli Gebrev <veli@innosys.co.za>
2023-08-25 11:50:34 +0200

    settings: Add bank account settings to display on EFT payment components

--- TTV.Web/Blazor/LocalConfig.cs
+++ TTV.Web/Blazor/LocalConfig.cs
@@ -9,4 +9,13 @@ public record LocalConfig
     public string PostLogoutRedirectUri { get; init; } = string.Empty;
     public string ResponseType { get; init; } = string.Empty;
     public string[] ApiScopes { get; init; } = Array.Empty<string>();
+    public BankAccountConfig BankAccount { get; init; } = new();
+}
+
+public record BankAccountConfig
+{
+    public string AccountNumber { get; init; } = string.Empty;
+    public string BankName { get; init; } = string.Empty;
+    public string BranchCode { get; init; } = string.Empty;
+    public string? Branch { get; init; }
 }

ed0c6671c1d09fa87130b2c6d3aa2491c1ec1865
Veli Gebrev <velislav@synatic.com>
2024-05-28 09:39:44 +0200

    feat: Add stylized landing page with intro, meet the teacher, and testimonials sections

---
+++ TTV.Web/Blazor/Pages/LandingPage/TestimonialInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace TTV.Web.Blazor.Pages.LandingPage;
+
+public record TestimonialInfo
+{
+    public string Text { get; init; } = string.Empty;
+    public string TestimonialBy { get; init; } = string.Empty;
+}
---
+++ TTV.Web/Blazor/wwwroot/js/app.js
@@ -0,0 +1,36 @@
+﻿const seconds = 1000;
+
+window.initializeCarousel = (carouselId) => {
+  const carouselElem = document.getElementById(carouselId);
+  new bootstrap.Carousel(carouselElem, {
+    interval: 10 * seconds,
+    ride: 'carousel'
+  });
+};
+
+window.initIntersectionObserver = (element) => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 1.0 });
+
+  observer.observe(element);
+};
+
+let interval;
+window.initLandingVideo = (videoId) => {
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => {
+    const video = document.getElementById(videoId);
+    if (!video) return;
+    video.pause();
+    video.currentTime = 0;
+    video.play();
+  }, 21 * seconds);
+}

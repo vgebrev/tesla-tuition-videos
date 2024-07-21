629c08d4f38323fc1d58d9ee203d72d213b7a1d5
Veli Gebrev <velislav@synatic.com>
2024-07-21 17:11:01 +0200

    chore: Bump version to 0.0.6

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.5")]
-[assembly: AssemblyFileVersion("0.0.5")]
-[assembly: AssemblyInformationalVersion("0.0.5")]
+[assembly: AssemblyVersion("0.0.6")]
+[assembly: AssemblyFileVersion("0.0.6")]
+[assembly: AssemblyInformationalVersion("0.0.6")]

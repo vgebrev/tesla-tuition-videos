8dcb95cb20934e943471ae4bbf7bd51b2ed83305
Veli Gebrev <velislav@synatic.com>
2024-08-02 15:32:00 +0200

    fix: Video player no longer incorrectly shows the previous lesson's video

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.7.4")]
-[assembly: AssemblyFileVersion("0.0.7.4")]
-[assembly: AssemblyInformationalVersion("0.0.7.4")]
+[assembly: AssemblyVersion("0.0.7.5")]
+[assembly: AssemblyFileVersion("0.0.7.5")]
+[assembly: AssemblyInformationalVersion("0.0.7.5")]

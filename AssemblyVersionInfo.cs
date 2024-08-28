e2f55f96cd3e4cbf1788ba3bd2a8f7cc058bda37
Veli Gebrev <velislav@synatic.com>
2024-08-28 22:26:08 +0200

    chore: Bump version to 1.1.0

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.3.1")]
-[assembly: AssemblyFileVersion("1.0.3.1")]
-[assembly: AssemblyInformationalVersion("1.0.3.1")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]

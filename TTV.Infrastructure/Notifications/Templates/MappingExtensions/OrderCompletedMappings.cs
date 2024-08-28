6e8279d98c4aec1d2ff5085c706041e0089c7732
Veli Gebrev <velislav@synatic.com>
2024-08-28 22:24:26 +0200

    fix: Add paid payments to order complete template data

--- TTV.Infrastructure/Notifications/Templates/MappingExtensions/OrderCompletedMappings.cs
+++ TTV.Infrastructure/Notifications/Templates/MappingExtensions/OrderCompletedMappings.cs
@@ -1,4 +1,5 @@
-﻿using TTV.Domain.Entities;
+﻿using TTV.Domain.DomainServices;
+using TTV.Domain.Entities;
 
 namespace TTV.Infrastructure.Notifications.Templates.MappingExtensions;
 internal static class OrderCompletedMappings
@@ -9,8 +10,13 @@ internal static class OrderCompletedMappings
            decimal.Round(order.OrderTotal, 2),
            decimal.Round(order.PaymentsTotal, 2),
            order.Lessons.Select(lesson => lesson.ToTemplateData(order.PlacedOn)).ToArray(),
-           order.AppliedVouchers.Select(x => x.ToTemplateData()).ToArray());
+           order.AppliedVouchers.Select(x => x.ToTemplateData()).Concat(order.Payments.Where(p => p.Status == PaymentStatus.Paid).Select(p => p.ToTemplateData())).ToArray());
 
     internal static OrderItem ToTemplateData(this OrderDiscountVoucher appliedDiscount)
         => new(appliedDiscount.VoucherId, $"Discount Voucher ({appliedDiscount.Voucher.Code})", decimal.Round(appliedDiscount.Amount, 2));
+
+    internal static OrderItem ToTemplateData(this Payment payment)
+        => new(payment.Id.GetHashCode(), $"{payment.Type.ToDisplayString()} Payment", decimal.Round(payment.Amount, 2));
+
+    
 }
--- TTV.Infrastructure/Notifications/Templates/MappingExtensions/OrderConfirmationMappings.cs
+++ TTV.Infrastructure/Notifications/Templates/MappingExtensions/OrderConfirmationMappings.cs
@@ -4,7 +4,7 @@ namespace TTV.Infrastructure.Notifications.Templates.MappingExtensions;
 internal static class OrderConfirmationMappings
 {
     internal static OrderConfirmationTemplate ToOrderConfirmationTemplateData(this Order order)
-        => new(order.Id, "Customer", decimal.Round(order.TotalAmount, 2), order.Lessons.Select(lesson => lesson.ToTemplateData(order.PlacedOn)).ToArray());
+        => new(order.Id, "Customer", decimal.Round(order.OrderTotal, 2), order.Lessons.Select(lesson => lesson.ToTemplateData(order.PlacedOn)).ToArray());
 
     internal static OrderItem ToTemplateData(this Lesson lesson, DateTime orderDate)
         => new(lesson.Id, lesson.Title, decimal.Round(lesson.PriceAt(orderDate).EffectiveAmount, 2));

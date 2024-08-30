679f7660ea2971297b6eb7544349be4927e2be3a
Veli Gebrev <velislav@synatic.com>
2024-08-30 13:28:26 +0200

    fix: Testimonials text scaling

--- TTV.Web/Svelte/src/components/LandingPage/Testimonial.svelte
+++ TTV.Web/Svelte/src/components/LandingPage/Testimonial.svelte
@@ -6,7 +6,7 @@
 <div class="row d-flex justify-content-center">
   <div class="col-12 col-sm-11 col-md-8 col-lg-9 col-xl-8">
     <div class="d-flex">
-      <div class="flex-grow-1">
+      <div class="flex-grow-1 align-items-center d-flex justify-content-center">
         <figure>
           <blockquote class="blockquote mb-4">
             <p>
@@ -22,3 +22,39 @@
     </div>
   </div>
 </div>
+
+<style>
+  figure {
+    transform: scale(0.75);
+  }
+
+  @media (min-width: 576px) {
+    figure {
+      transform: scale(0.82);
+    }
+  }
+
+  @media (min-width: 768px) {
+    figure {
+      transform: scale(0.9);
+    }
+  }
+
+  @media (min-width: 992px) {
+    figure {
+      transform: scale(1);
+    }
+  }
+
+  @media (min-width: 1200px) {
+    figure {
+      transform: scale(1);
+    }
+  }
+
+  @media (min-width: 1400px) {
+    figure {
+      transform: scale(1);
+    }
+  }
+</style>
